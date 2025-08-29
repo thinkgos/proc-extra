d0d49a6c756a32a89b8581078cbf222a2edd4f44
thinkgo <thinkgo@aliyun.com>
2025-08-29 15:15:09 +0800

    fix: fix sensitive

--- sensitive/privacy_default.go
+++ sensitive/privacy_default.go
@@ -1,24 +1,59 @@
 package sensitive
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 var (
 	privacyRandom    = New()
 	privacyTimestamp = New(WithIvGen(IvGenTimestamp), WithIvChecker(IvCheckerTimestamp(time.Minute*5)))
 )
 
-func EncryptPrivacyRandom(secret, rawText []byte) (string, error) {
+func EncryptViaRandom(secret, rawText []byte) (string, error) {
 	return privacyRandom.Encrypt(secret, rawText)
 }
 
-func DecryptPrivacyRandom(secret []byte, cipherText string) ([]byte, error) {
+func DecryptViaRandom(secret []byte, cipherText string) ([]byte, error) {
 	return privacyRandom.Decrypt(secret, cipherText)
 }
 
-func EncryptPrivacyTimestamp(secret, rawText []byte) (string, error) {
+func EncryptObjectViaRandom(secret []byte, data any) (string, error) {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	return EncryptViaRandom(secret, b)
+}
+
+func DecryptObjectViaRandom(secret []byte, cipherText string, v any) error {
+	bs, err := DecryptViaRandom(secret, cipherText)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bs, v)
+}
+
+func EncryptViaTimestamp(secret, rawText []byte) (string, error) {
 	return privacyTimestamp.Encrypt(secret, rawText)
 }
 
-func DecryptPrivacyTimestamp(secret []byte, cipherText string) ([]byte, error) {
+func DecryptViaTimestamp(secret []byte, cipherText string) ([]byte, error) {
 	return privacyTimestamp.Decrypt(secret, cipherText)
+}
+
+func EncryptObjectViaTimestamp(secret []byte, data any) (string, error) {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return "", err
+	}
+	return EncryptViaTimestamp(secret, b)
+}
+
+func DecryptObjectViaTimestamp(secret []byte, cipherText string, v any) error {
+	b, err := DecryptViaTimestamp(secret, cipherText)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, v)
 }
