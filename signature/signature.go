3005140faee43260094a7a8b05f1d55dd9faddff
thinkgo <thinkgo@aliyun.com>
2025-09-26 16:02:11 +0800

    fix: fix signature

--- signature/signature.go
+++ signature/signature.go
@@ -1,14 +1,37 @@
 package signature
 
 import (
-	"errors"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+	"hash"
 )
 
-// error defined
-var (
-	ErrInputInvalidLength     = errors.New("encoded message length must be more than zero")
-	ErrInputNotMoreABlock     = errors.New("decoded message length must be more than a block size")
-	ErrInputNotMultipleBlocks = errors.New("decoded message length must be multiple of block size")
-	ErrInvalidIvSize          = errors.New("iv length must equal block size")
-	ErrUnPaddingOutOfRange    = errors.New("unPadding out of range")
-)
+// Hmac Generate a hex hash value with the key,
+// expect: hmacmd5, hmacsha1, hmacsha224, hmacsha256, hmacsha384, hmacsha512.
+// default: hmacsha256
+func Hmac(method string, key, planText []byte) []byte {
+	var h func() hash.Hash
+
+	switch method {
+	case "hmacmd5":
+		h = md5.New
+	case "hmacsha1":
+		h = sha1.New
+	case "hmacsha224":
+		h = sha256.New224
+	case "hmacsha256":
+		h = sha256.New
+	case "hmacsha384":
+		h = sha512.New384
+	case "hmacsha512":
+		h = sha512.New
+	default:
+		h = sha512.New
+	}
+	hasher := hmac.New(h, key)
+	_, _ = hasher.Write(planText)
+	return hasher.Sum(nil)
+}
